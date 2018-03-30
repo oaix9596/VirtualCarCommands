7794d2ef2c01cc10e7a6e5642ce8d05a17b8324e
oaix9596 <oaix@umich.edu>
2018-03-30 10:58:10 -0400

    First Commit, this is a program that sends a message that the fake BSM will parse into distance between the cars, the velocity of the virtual car, and the rate of change of distance between the cars

---
+++ VirtualCarCommands/Publish/build/CMakeFiles/polysync-publisher-cpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/polysync-publisher-cpp.dir/Publish.cpp.o"
+  "polysync-publisher-cpp.pdb"
+  "polysync-publisher-cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/polysync-publisher-cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ VirtualCarCommands/build/CMakeFiles/polysync-publisher-subscriber-cpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/polysync-publisher-subscriber-cpp.dir/PublishSubscribe.cpp.o"
+  "polysync-publisher-subscriber-cpp.pdb"
+  "polysync-publisher-subscriber-cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/polysync-publisher-subscriber-cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
